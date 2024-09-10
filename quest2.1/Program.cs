e68eed84bcfe29d4fb771dd4a480c194dfab1af8
Bohdan <bogdankors1@gmail.com>
2024-09-11 00:16:44 +0300

    Lab 1 and a half of lab 2

---
+++ quest2.1/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Text;
+
+class Program
+{
+
+    static void Main()
+    {
+        Console.OutputEncoding = Encoding.UTF8;
+        Console.InputEncoding = Encoding.UTF8;
+
+        string[] firstarea = Console.ReadLine().Split(' ');
+        string[] secondarea = Console.ReadLine().Split(' ');
+
+          int leftMatch = FindCommonLeftEnd(firstarea, secondarea);
+
+           int rightMatch = FindCommonRightEnd(firstarea, secondarea);
+
+                    if (leftMatch > rightMatch && leftMatch > 0)
+                    {
+                        Console.WriteLine($"Найбільний спільний кінець зліва - {leftMatch}");
+                    }
+                    else if (rightMatch > leftMatch && rightMatch > 0)
+                    {
+                        Console.WriteLine($"Найбільний спільний кінець зправа - {rightMatch}");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Ні з одної з сторін немає спільних слів");
+                    }
+
+                    static int FindCommonLeftEnd(string[] firstarea, string[] secondarea)
+                    {
+                        int minLenght = Math.Min(firstarea.Length, secondarea.Length);
+                        int count = 0;
+
+                        for (int i = 0; i < minLenght; i++)
+                        {
+                            if (firstarea[i] == secondarea[i])
+                            {
+                                count++;
+                            }
+                            else
+                            {
+                                break;
+                            }
+                        }
+                        return count;
+                    }
+                    static int FindCommonRightEnd(string[] firstarea, string[] secondarea)
+                    {
+                        int minLength = Math.Min(firstarea.Length, secondarea.Length);
+                        int count = 0;
+
+                        for (int i = 0; i < minLength; i++)
+                        {
+                            if (firstarea[firstarea.Length - 1 - i] == secondarea[secondarea.Length - 1 - i])
+                            {
+                                count++;
+                            }
+                            else
+                            {
+                                break;
+                            }
+                        }
+                        return count;
+                    }
+
+    }
+}
---
+++ quest2.2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Text;
+
+class Program
+{
+    static void Main()
+    {
+        Console.OutputEncoding = Encoding.UTF8;
+        Console.InputEncoding = Encoding.UTF8;
+
+        Console.WriteLine("Введіть числа для масиву: ");
+        int[] array = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+        Console.WriteLine("Введіть кількість обертів k: ");
+        int k = int.Parse(Console.ReadLine());
+
+        int[] summary = new int[array.Length];
+
+        for (int rott = 0; rott < k; rott++)
+        {
+            array = RotateRight(array);
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                summary[i] += array[i];
+            }
+        }
+        Console.WriteLine("Summary: " + string.Join(" ", summary));
+    }
+
+    static int[] RotateRight(int[] array)
+    {
+        int[] rotted = new int[array.Length];
+        rotted[0] = array[array.Length - 1];
+
+        for (int i = 1; i < array.Length; i++)
+        {
+            rotted[i] = array[i - 1];
+        }
+
+        Console.WriteLine("Rotate: " + string.Join(" ", rotted));
+
+        return rotted;
+    }
+}
