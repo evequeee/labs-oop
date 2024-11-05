fb41e0c5d64ce6765c3c23a20c0be01658630913
eve que <bogdankors1@gmail.com>
2024-11-05 22:15:25 +0200

    tired af

---
+++ quest7.1/method/Task7_01_in.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task7_01_in
+{
+    public interface ToCall
+    {
+        void Call(string number);
+    }
+
+    public interface Browser
+    {
+        void Browse(string url);
+    }
+
+    public class Smartphone : ToCall, Browser
+    {
+        public void Call(string number)
+        {
+            bool isAllDigits = true;
+            foreach (char y in number)
+            {
+                if (!char.IsDigit(y))
+                {
+                    isAllDigits = false;
+                    break;
+                }
+            }
+            if (number.All(char.IsDigit))
+            {
+                Console.WriteLine($"Calling... {number}");
+            }
+            else
+            {
+                Console.WriteLine("Invalid number");
+            }
+        }
+
+        public void Browse(string url)
+        {
+            bool nodigit = true;
+            foreach (char y in url)
+            {
+                if (char.IsDigit(y))
+                {
+                    nodigit = false;
+                    break;
+                }
+            }
+            if (nodigit) Console.WriteLine($"Browsing: {url}");
+
+            else Console.WriteLine("Invalid URL");
+
+        }
+    }
+}
